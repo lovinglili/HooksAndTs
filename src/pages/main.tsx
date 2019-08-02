478f978bc87c5139fba853066f6e8e64268cd027
“liqing” <wb-lq507853@antfin.com>
2019-08-02 19:04:23 +0800

    feat: 初试hooks、ts

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,23 +1,68 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import React,{ useState } from 'react';
-import  styles from './main.module.less';
-
-export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
+import React,{ useState, useEffect } from 'react';
+const   styles =require ('./main.module.less');
+interface Props {
+  aa: string;
+  bb: string;
 }
 
-function Example({ name, enthusiasmLevel = 1 }: Props) {
+function Example(props:Props) {
   const [count,setCount] = useState(1);
+  useEffect(()=>{
+    fetch('/api/xiaoyi/goodsList').then(res=>res.json()).then(data=>{
+      console.log(data,'data')
+    })
+  })
+  console.log(styles,'styles')
   return (
     <>
     <div className={styles.container}>you clicked {count} times</div>
-    <button onClick={()=>setCount(count+1)}>{name}</button>
-    <button onClick={()=>setCount(count-1)}>{enthusiasmLevel}</button>
+    <button onClick={()=>setCount(count+1)}>add</button>
+    <button onClick={()=>setCount(count-1)}>reduce</button>
 
   </>
   );
 }
 
-export default Example;
+function A(){
+  let aa = "ads"
+  return <Example aa={aa} bb='ss'/>
+}
+
+export default A;
+
+// class Example extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   componentDidMount(){
+//     fetch('/api/xiaoyi/goodsList').then(res=>res.json()).then(data=>{
+//       console.log(data,'data')
+//     })
+//   }
+//   getstatus(){
+
+//   }
+
+//   getStatus(){
+
+//   }
+
+// // TODO:
+// // FIXME:
+//   render() {
+//     return (
+//       <>
+//         <div>You clicked {this.state.count} times</div>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>add</button>
+//         <button onClick={() => this.setState({ count: this.state.count - 1 })}>reduce</button>
+//       </>
+//     );
+//   }
+// }
+// export default Example
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/xiaoyi', { target: 'http://localhost:8989/' }));
+  app.use(proxy('/api', { 
+    target: 'http://188.131.191.92',
+    changeOrigin: true,  //是否跨域
+    pathRewrite: {
+      '^/api': ''   //重写接口
+    }
+  }));
 };
